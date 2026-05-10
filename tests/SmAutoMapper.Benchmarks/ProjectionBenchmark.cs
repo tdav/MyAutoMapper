07f95ab4e94defeb102967ce6f22e3245734c479
admin <tdav@mail.ru>
2026-05-10 18:36:26 +0500

    bench: add ParameterizedProjectionBenchmark to track injector pool perf

--- tests/SmAutoMapper.Benchmarks/ProjectionBenchmark.cs
+++ tests/SmAutoMapper.Benchmarks/ProjectionBenchmark.cs
@@ -59,3 +59,51 @@ public class ProjectionBenchmark
         return result.Count;
     }
 }
+
+[MemoryDiagnoser]
+public class ParameterizedProjectionBenchmark
+{
+    private IProjectionProvider provider = null!;
+    private ParameterBinder binder = null!;
+
+    public sealed class ScoreSource
+    {
+        public int Score { get; set; }
+    }
+
+    public sealed class ScoreDest
+    {
+        public int Score { get; set; }
+        public bool Passes { get; set; }
+    }
+
+    public sealed class ScoreProfile : MappingProfile
+    {
+        private readonly ParameterSlot<int> minScore;
+
+        public ScoreProfile()
+        {
+            minScore = DeclareParameter<int>("MinScore");
+
+            CreateMap<ScoreSource, ScoreDest>()
+                .ForMember(d => d.Passes, opt => opt.MapFrom<int>(minScore, (src, threshold) => src.Score >= threshold));
+        }
+    }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        var builder = new MappingConfigurationBuilder();
+        builder.AddProfile<ScoreProfile>();
+        var config = builder.Build();
+        this.provider = config.CreateProjectionProvider();
+        this.binder = new ParameterBinder();
+        this.binder.Set<int>("MinScore", 50);
+    }
+
+    [Benchmark]
+    public object GetProjection()
+    {
+        return this.provider.GetProjection<ScoreSource, ScoreDest>(this.binder);
+    }
+}
